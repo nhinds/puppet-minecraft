7f416276c203ef0cfabb90bb5712bef36cfa2ba3
Nicholas Hinds <hindsn@gmail.com>
2017-05-28 21:25:15 -0700

    Wait for minecraft to start before managing settings

--- lib/puppet/provider/minecraft_setting/screen.rb
+++ lib/puppet/provider/minecraft_setting/screen.rb
@@ -1,11 +1,16 @@
 require 'json'
+require 'timeout'
 
 Puppet::Type.type(:minecraft_setting).provide(:screen) do
   def value
+    # If the file does not currently exist, assume it will be empty. Do not wait for this file to be created in case we are in no-op mode
+    return [] unless File.exist? resource[:filename]
     JSON.load(File.new(resource[:filename])).map { |o| o[resource[:id_property]] }
   end
 
   def value=(newvalue)
+    # Wait for the file to exist before accessing its value in case Minecraft is still starting up.
+    wait_until_exists!
     current = value
     (current - newvalue).each do |to_remove|
       Puppet.debug "Need to remove #{to_remove}"
@@ -21,5 +26,16 @@ Puppet::Type.type(:minecraft_setting).provide(:screen) do
   def screencmd(cmd)
     execute(['screen', '-p', '0', '-S', resource[:instance], '-X', 'eval', %Q(stuff "#{cmd}"\\015)],
 	    failonfail: true, uid: resource[:user])
+  end
+
+  def wait_until_exists!
+    Timeout.timeout(60) do
+      until File.exist? resource[:filename]
+        Puppet.info "#{resource[:filename]} does not yet exist, waiting for Minecraft to create it..."
+        sleep 5
+      end
+    end
+  rescue Timeout::Error
+    raise Puppet::Error, "#{resource[:filename]} does not exist after 60 seconds, cannot manage settings"
   end
 end
